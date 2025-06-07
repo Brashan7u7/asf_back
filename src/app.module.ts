321517a8506b863ce39e6878383d942fa889e448
Brashan7u7 <84888260+Brashan7u7@users.noreply.github.com>
2025-06-07 13:54:46 -0600

    change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { AuditoriaModule } from './auditoria/auditoria.module';
-import { NoticiaModule } from './noticia/noticia.module';
-import { NotificacionModule } from './notificacion/notificacion.module';
-import { AuthModule } from './auth/auth.module';
-import { AppService } from './app.service';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth/auth.guard';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
+import { AuditoriaModule } from "./auditoria/auditoria.module";
+import { NoticiaModule } from "./noticia/noticia.module";
+import { NotificacionModule } from "./notificacion/notificacion.module";
+import { AuthModule } from "./auth/auth.module";
+import { AppService } from "./app.service";
+import { APP_GUARD } from "@nestjs/core";
+import { AuthGuard } from "./auth/auth.guard";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'asf_db',
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || "5432"),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       logging: true,
       autoLoadEntities: true,
       synchronize: true,
@@ -27,7 +31,7 @@ import { AuthGuard } from './auth/auth.guard';
     AuditoriaModule,
     NoticiaModule,
     NotificacionModule,
-    AuthModule
+    AuthModule,
   ],
   providers: [
     AppService,
@@ -37,4 +41,4 @@ import { AuthGuard } from './auth/auth.guard';
     },
   ],
 })
-export class AppModule { }
+export class AppModule {}
